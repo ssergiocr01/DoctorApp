161941bfbfd073bbd9b893c45164288b8f0bb8fa
Sergio Serrano <ssergiocr01@yahoo.com>
2024-07-20 17:00:06 -0600

    AgregadoFormularioLogin

---
+++ DoctorAppFrontend/src/app/compartido/compartido.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Sesion } from '../usuario/interfaces/sesion';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class CompartidoService {
+
+  constructor(private _snackBar: MatSnackBar) { }
+
+  mostrarAlerta(mensaje: string, tipo: string) {
+    this._snackBar.open(mensaje, tipo, {
+      horizontalPosition: "end",
+      verticalPosition: "top",
+      duration: 3000
+    })
+  }
+
+  guardarSesion(sesion: Sesion) {
+    localStorage.setItem("usuarioSesion", JSON.stringify(sesion.username));
+  }
+
+  obtenerSesion() {
+    const sesionString = localStorage.getItem("usuarioSesion");
+    const usuarioSesion = JSON.parse(sesionString!);
+    return usuarioSesion;
+  }
+
+  eliminarSesion(){
+    localStorage.removeItem("usuarioSesion");
+  }
+}
